5b96ced0ee2ac0fc997e0e438ea5a6e2afd903e2
ringggr <ringggr@icloud.com>
2018-09-22 14:19:28 +0800

    completed DATINGTEST

--- chapter1/kNN.py
+++ chapter1/kNN.py
@@ -66,6 +66,20 @@ def datingClassTest():
 
 
 
+def classifyPerson():
+	resultList = ['not at all', 'in small doses', 'in large doses']
+	percentTats = float(raw_input("percentage of time spent playing video games?"))
+	ffMiles = float(raw_input("frequent flier miles earned per year?"))
+	iceCream = float(raw_input("liters of ice cream consumed per year?"))
+	datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	inArr = array([ffMiles, percentTats, iceCream])
+	classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 5)
+	print "You will probably like this person: ", resultList[classifierResult - 1]
+
+
+
+
 
 
 
