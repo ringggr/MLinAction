4ea4b37323bfda848689e886f7a195a4108597a5
ringggr <ringggr@icloud.com>
2018-09-25 23:44:37 +0800

    no message

--- chapter2/trees.py
+++ chapter2/trees.py
@@ -31,6 +31,43 @@ def splitDataSet(dataSet, axis, value):
 	for featVec in dataSet:
 		if featVec[axis] == value:
 			reducedFeatVec = featVec[:axis]
-			reducedFeatVec.extend(featVec[axis + 1])
+			reducedFeatVec.extend(featVec[axis + 1:])
 			retDataSet.append(reducedFeatVec)
-	return retDataSe
+	return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+	numFeatures = len(dataSet[0]) - 1
+	baseEntropy = calcShannonEnt(dataSet)
+	bestInfoGain = 0.0;
+	bestFeature = -1
+	for i in range(numFeatures):
+		featList = [example[i] for example in dataSet]
+		uniqueVals = set(featList)
+		newEntropy = 0.0
+		for value in uniqueVals:
+			subDataSet = splitDataSet(dataSet, i, value)
+			prob = len(subDataSet) / float(len(dataSet))
+			newEntropy += prob * calcShannonEnt(subDataSet)
+		infoGain = baseEntropy - newEntropy
+		if (infoGain > bestInfoGain):
+			bestInfoGain = infoGain
+			bestFeature = i
+	return bestFeature
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
