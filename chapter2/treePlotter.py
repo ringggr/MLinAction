dd180022bf023649e0427985a4c223b12a18fe43
ringggr <ringggr@icloud.com>
2018-09-27 23:52:32 +0800

    corrected mistake, do not use chinese.

--- chapter2/treePlotter.py
+++ chapter2/treePlotter.py
@@ -16,6 +16,6 @@ def createPlot():
 	fig = plt.figure(1, facecolor = 'white')
 	fig.clf()
 	createPlot.ax1 = plt.subplot(111, frameon = False)
-	plotNode('决策节点', (0.5, 0.1), (0.1, 0.5), decisionNode)
-	plotNode('叶节点', (0.8, 0.1), (0.3, 0.8), leafNode)
+	plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+	plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
 	plt.show()
